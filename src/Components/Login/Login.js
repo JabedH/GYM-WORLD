8fe356229b8674b0710e3f2f44b9566e8bfc09cc
JabedH <87475551+JabedH@users.noreply.github.com>
2022-04-17 10:30:28 +0600

    complete login desing

--- src/Components/Login/Login.js
+++ src/Components/Login/Login.js
@@ -1,14 +1,45 @@
 import React from "react";
+import "./Login.css";
+import { Form } from "react-bootstrap";
+import { Link } from "react-router-dom";
+import bg from "../../img/bg.jpg";
 
 const Login = () => {
   return (
-    <div>
-      <form action="">
-        <input type="text" />
-        <input type="email" name="" id="" />
-        <input type="password" name="password" id="" />
-        <input type="password" name="confirmPassword" id="" />
-      </form>
+    <div className="login-bg">
+      <img src={bg} alt="" />
+      <div>
+        <Form className="mx-auto text-start">
+          <input
+            className="w-100 mt-3"
+            type="email"
+            name="email"
+            placeholder="Enter email"
+            required
+          />
+          <input
+            className="w-100 mt-3 pl-2"
+            type="password"
+            name="password"
+            placeholder="Password"
+            required
+          />
+          {/* <input
+          className="w-100 mt-3"
+          type="password"
+          name="ConfirmPassword"
+          placeholder="ConfirmPassword"
+        /> */}
+          <button className="w-100 btn-color mt-3" type="submit">
+            Submit
+          </button>
+          <div className="mt-2 text-center ">
+            <Link to="/login" className="text-decoration-none account ">
+              Already you have an account?
+            </Link>
+          </div>
+        </Form>
+      </div>
     </div>
   );
 };
