e0f8b8e6303135a52e8eaf1333958265d1b63943
JabedH <87475551+JabedH@users.noreply.github.com>
2022-04-17 12:06:05 +0600

    added signup and sign buttton

--- src/Components/Login/Login.js
+++ src/Components/Login/Login.js
@@ -1,56 +1,67 @@
 import React from "react";
 import "./Login.css";
 import { Form } from "react-bootstrap";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import bg from "../../img/bg.jpg";
 import Sociallogin from "../Sociallogin/Sociallogin";
 
 const Login = () => {
+  const navigate = useNavigate();
+  const handleSignUp = () => {
+    navigate("Signup");
+    console.log("i got it");
+  };
   return (
     <div className="login-bg">
       <img src={bg} alt="" />
-      <div>
-        <Form className="mx-auto text-start">
-          <input
-            className="w-100 mt-3"
-            type="email"
-            name="email"
-            placeholder="Enter email"
-            required
-          />
-          <input
-            className="w-100 mt-3 pl-2"
-            type="password"
-            name="password"
-            placeholder="Password"
-            required
-          />
-          {/* <input
+      <div className="form-padding ">
+        <div className="full-form">
+          <Form className=" text-start">
+            <input
+              className="w-100 mt-3"
+              type="email"
+              name="email"
+              placeholder="Enter email"
+              required
+            />
+            <input
+              className="w-100 mt-3 pl-2"
+              type="password"
+              name="password"
+              placeholder="Password"
+              required
+            />
+            {/* <input
           className="w-100 mt-3"
           type="password"
           name="ConfirmPassword"
           placeholder="ConfirmPassword"
         /> */}
-          <button className="w-100 btn-color mt-3" type="submit">
-            Submit
-          </button>
-          <div className="mt-2 text-center ">
-            <Link to="/login" className="text-decoration-none account ">
-              Already you have an account?
-            </Link>
-          </div>
-          <div className="mt-2 text-center ">
-            <Link to="/login" className="text-decoration-none account ">
-              Forget Password?
-            </Link>
-          </div>
-          <div className="social-line">
-            <dir className="or-line"></dir>
-            <p>Or</p>
-            <dir className="or-line"></dir>
-          </div>
+            <button className="w-100 btn-color mt-3" type="submit">
+              Submit
+            </button>
+            <div className="mt-2 text-center ">
+              <Link
+                to="/Signup"
+                onClick={handleSignUp}
+                className="text-decoration-none account "
+              >
+                Already you have an account?
+              </Link>
+            </div>
+            <div className="mt-2 text-center ">
+              <Link to="/login" className="text-decoration-none account ">
+                Forget Password?
+              </Link>
+            </div>
+            <div className="social-line">
+              <dir className="or-line"></dir>
+              <p>Or</p>
+              <dir className="or-line"></dir>
+            </div>
+          </Form>
           <Sociallogin />
-        </Form>
+        </div>
       </div>
     </div>
   );
--- src/Components/Signup/Signup.js
+++ src/Components/Signup/Signup.js
@@ -2,50 +2,61 @@ import React from "react";
 import { Form } from "react-bootstrap";
 import { Link } from "react-router-dom";
 import bg from "../../img/bg.jpg";
+import Sociallogin from "../Sociallogin/Sociallogin";
 import "./Signup.css";
 
 const Signup = () => {
   return (
     <div className="login-bg">
       <img src={bg} alt="" />
-      <div>
-        <Form className="mx-auto text-start">
-          <input
-            className="w-100"
-            type="text"
-            name="name"
-            placeholder="Enter Name"
-            required
-          />
-          <input
-            className="w-100 mt-3"
-            type="email"
-            name="email"
-            placeholder="Enter email"
-            required
-          />
-          <input
-            className="w-100 mt-3 pl-2"
-            type="password"
-            name="password"
-            placeholder="Password"
-            required
-          />
-          <input
-            className="w-100 mt-3"
-            type="password"
-            name="ConfirmPassword"
-            placeholder="ConfirmPassword"
-          />
-          <button className="w-100 btn-color mt-3" type="submit">
-            Submit
-          </button>
-          <div className="mt-2 text-center ">
-            <Link to="/login" className="text-decoration-none account ">
-              Already you have an account?
-            </Link>
-          </div>
-        </Form>
+      <div className="form-padding container">
+        <div className="full-form">
+          <Form className=" text-start">
+            <input
+              className="w-100 mt-3"
+              type="text"
+              name="name"
+              id=""
+              placeholder="your name"
+              required
+            />
+            <input
+              className="w-100 mt-3"
+              type="email"
+              name="email"
+              placeholder="Enter email"
+              required
+            />
+            <input
+              className="w-100 mt-3 pl-2"
+              type="password"
+              name="password"
+              placeholder="Password"
+              required
+            />
+            {/* <input
+          className="w-100 mt-3"
+          type="password"
+          name="ConfirmPassword"
+          placeholder="ConfirmPassword"
+        /> */}
+            <button className="w-100 btn-color mt-3" type="submit">
+              Submit
+            </button>
+            <div className="mt-2 text-center ">
+              <Link to="/login" className="text-decoration-none account ">
+                Already you have an account?
+              </Link>
+            </div>
+
+            <div className="social-line">
+              <dir className="or-line"></dir>
+              <p>Or</p>
+              <dir className="or-line"></dir>
+            </div>
+          </Form>
+          <Sociallogin />
+        </div>
       </div>
     </div>
   );
